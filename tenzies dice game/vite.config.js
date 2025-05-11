67720208aa40e680fc0931d6bb7b810656e20bd1
airatt88 <ayratmurtazayev@gmail.com>
2025-05-11 15:03:34 +0500

    Update vite.config.js

--- tenzies dice game/vite.config.js
+++ tenzies dice game/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/programming/tenzies-dice-game/',
+  base: '/programming/tenzies%20dice%20game/',
   plugins: [react()],
 })
